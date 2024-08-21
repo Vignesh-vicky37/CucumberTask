b36a2c9c66cdc8d79324a81fdd1ff48ac0407be8
Vignesh-vicky37 <3vicky@gmail.com>
2024-08-21 14:33:21 +0530

    test re runner class updated

--- src/test/java/org/base/TestReRunnerClass.java
+++ src/test/java/org/base/TestReRunnerClass.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="@C:\\Users\\JUJU\\eclipse-workspace\\CucumberTask\\src\\test\\resources\\Failed Scenario\\failed.txt",
+@CucumberOptions(features="@src\\test\\resources\\Failed Scenario\\failed.txt",
 				 glue="org.stepdefinition",
 				 monochrome=true)
 public class TestReRunnerClass {
